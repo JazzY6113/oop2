bc26a8ad98316f433e59fc29f821ac8636fe56ca
JazzY6113 <kbogomolov22@gmail.com>
2024-12-08 23:27:34 +0300

    2.3 доп задание

--- designpro/main/models.py
+++ designpro/main/models.py
@@ -1,3 +1,5 @@
+# models.py
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
@@ -19,21 +21,27 @@ class Category(models.Model):
         return self.name
 
 class Application(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    categories = models.ManyToManyField(Category)
+    image = models.ImageField(upload_to='applications/', validators=[validate_image], blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    comment = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return self.title
+
+class CategoryStatus(models.Model):
     STATUS_CHOICES = [
         ('new', 'Новая'),
         ('in_progress', 'Принято в работу'),
         ('completed', 'Выполнено'),
     ]
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    categories = models.ManyToManyField(Category)
-    image = models.ImageField(upload_to='applications/', validators=[validate_image], blank=True, null=True)
+    application = models.ForeignKey(Application, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='new')
-    created_at = models.DateTimeField(auto_now_add=True)
-    comment = models.TextField(blank=True, null=True)
 
     def __str__(self):
-        return self.title
+        return f"{self.application.title} - {self.category.name}: {self.get_status_display()}"
